5af079706f6143b8b70d9158bf84a4cb11984864
dheeraj <dnarasi1@binghamton.edu>
2020-06-11 16:52:07 -0400

    Merge Sort

--- SortingAlgorithms/src/sortingalgorithms/MergeSort.java
+++ SortingAlgorithms/src/sortingalgorithms/MergeSort.java
@@ -1,4 +1,33 @@
 
 public class MergeSort {
-	
+	public void merge(int[] unsorted, int[] left, int[] right) {
+		int l = left.length;
+		int r = right.length;
+		int i =0; int j=0; int k=0;
+		while(i<l && j<r) {
+			unsorted[k++] = (left[i]<right[j])?left[i++]:right[j++];
+		}
+		while(j<r) {
+			unsorted[k++]=right[j++];
+		}
+		while(i<l) {
+			unsorted[k++]=left[i++];
+		}
+	}
+	public void mergeSort(int unsorted[]) {
+		int length = unsorted.length;
+		if(length<2) return;
+		int mid = length/2;
+		int left[] = new int[mid];
+		int right[] = new int[(length-mid)];
+		for (int i = 0; i < mid; i++) {
+			left[i] = unsorted[i];
+		}
+		for (int i = mid; i < length; i++) {
+			right[i-mid] = unsorted[i];
+		}
+		mergeSort(left);
+		mergeSort(right);
+		merge(unsorted, left, right);
+	}
 }
--- SortingAlgorithms/src/sortingalgorithms/SortingAlgorithms.java
+++ SortingAlgorithms/src/sortingalgorithms/SortingAlgorithms.java
@@ -5,13 +5,15 @@ public class SortingAlgorithms {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int unsorted[] = {98,34,56,23,65};
+        int unsorted[] = {23,54,12,67,26,9,87,100};
         //SelectionSort select = new SelectionSort();
         //BubbleSort bub = new BubbleSort();
-        InsertionSort ins = new InsertionSort();
-        ins.insert(unsorted, unsorted.length);
+        //InsertionSort ins = new InsertionSort();
+        //ins.insert(unsorted, unsorted.length);
         //bub.bubble(unsorted, unsorted.length);
         //select.selection(unsorted, unsorted.length);
+        MergeSort sort = new MergeSort();
+        sort.mergeSort(unsorted);
         for(int i =0; i<unsorted.length;i++){
             System.out.println(unsorted[i]);
         }
